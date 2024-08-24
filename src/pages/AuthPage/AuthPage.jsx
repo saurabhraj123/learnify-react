f8c309650b88020d5794dbabe694ea037175cde2
saurabhraj123 <me4saurabh4u@gmail.com>
2024-08-25 01:14:51 +0530

    Add sign in / signup buttons

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,10 +1,5 @@
-// internal
 import PropTypes from "prop-types";
-
-// internal
 import { useTopLoadingBar } from "../../hooks";
-
-// css
 import classes from "./AuthPage.module.css";
 
 const AuthPage = (props) => {
@@ -13,9 +8,27 @@ const AuthPage = (props) => {
   // to show the top loading bar
   useTopLoadingBar({ setTopBarProgress, type });
 
+  const headingLabel =
+    type === "LOGIN"
+      ? "Sign in with Google to continue"
+      : "Create an account to continue";
+
   return (
     <div className={classes.container}>
-      {type === "LOGIN" ? "Log in page" : "Sign up page"}
+      <div className={classes.authContainer}>
+        <h3 className={classes.heading}>{headingLabel}</h3>
+        <button
+          className={classes.googleButton}
+          // onClick={() => signIn("google")}
+        >
+          <img
+            src="/google.png"
+            className={classes.googleIcon}
+            alt="Google logo"
+          />
+          Sign {type === "LOGIN" ? "in" : "up"} with Google
+        </button>
+      </div>
     </div>
   );
 };
